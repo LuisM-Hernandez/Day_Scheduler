665b5a82438d5e0869d711d0f2e0012edee6a4b5
Luis <m.luishernandez@outlook.com>
2021-07-18 19:16:13 -0400

    timeblocks function finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,33 @@
 $(document).ready(function () {
 
     //Display the current day
-    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+    // $("#currentDay").text(moment().format("dddd, MMMM Do"));
+    $("#currentDay").text(moment().hours());
 
-    function hours() {
-        var currentHours = moment().hours();
-        console.log(currentHours);
+
+    function timeBlocks() {
+        var currentHour = moment().hours();
+        console.log(currentHour);
 
         $(".time-block").each(function () {
-            var blockHour = $(this).attr("id");
-            console.log(blockHour);
-            $(this).addClass("past");
-            
+            // console.log(this);
+            var blockHours = parseInt($(this).attr("id").split("-")[1])
+            console.log(blockHours);
+
+            if (currentHour === blockHours) {
+                $(this).addClass("present");
+            }
+            else if (currentHour < blockHours) {
+                $(this).addClass("future");
+            }
+            else{
+                $(this).addClass("past")
+
+            }    
         })
     }
 
-    hours()
+    timeBlocks()
 
 })
 
